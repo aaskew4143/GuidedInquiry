7a1393cade90c3821854982b94af07530f5bd413
Antonio Askew <antonioaskew@AdminisatorsMBP.lan>
2023-01-09 17:52:56 -0500

    submit

---
+++ guided-inquiry2/easy/easy.js
@@ -0,0 +1,21 @@
+var name1 = "Thomas";
+
+var name2 = "John";
+
+if (name1.length > name2.length) {
+   
+    console.log(
+ "The name" + name1 + " is longer than " + name2 + " by " + (name1.length - name2.length)
+    
+    );
+
+
+} else if (name1. length <name2.length) {
+
+    console.log(
+        "The name " + name2 + " is longer than " + name1 + "by " + (name2.length - name1.length )
+    );
+
+} else {
+    console.log ("Name 1 is larger by 2 characters")
+}
---
+++ guided-inquiry2/very easy/veryeasy.js
@@ -0,0 +1,5 @@
+var a = 3
+var b = 4
+
+
+console.log('This is the difference between a * b is: '12)
